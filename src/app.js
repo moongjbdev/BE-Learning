480eba98d921462a415f02866a1adc8b9bc3eb3b
moongjbdev <moongjbdev@gmail.com>
2024-07-30 00:30:42 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'))
+
+
+// Middleware 
+    
+// Database
+
+//Routes
+
+// Handle Error
+
+module.exports = app;
