76dcc7d6ae8162e375fdbfdd7741036d0a00b3f4
moongjbdev <moongjbdev@gmail.com>
2024-07-30 13:48:26 +0700

    install packages

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
-app.use(morgan('dev'))
+const {default: helmet} = require('helmet');
+const compression = require('compression')
 
 
 // Middleware 
+app.use(morgan('combined'))
+app.use(helmet())
+app.use(compression())
     
 // Database
 
 //Routes
-
+app.get('/', (req, res) => {
+   return res.status(404).json({
+    message: "Hello, world!"
+   })
+})
 // Handle Error
 
 module.exports = app;
